c77999e074415534b35763ca9d9af3818dde7da0
Abraham <abrahancho6@hotmail.com>
2018-12-23 16:54:31 -0430

    Incorpora pronps y states.

--- src/components/App.js
+++ src/components/App.js
@@ -6,11 +6,13 @@ import Header from './global/Header'
 import Content from './global/Content'
 import Footer from './global/Footer'
 
+var items=['opcion1','opcion2','opcion3','opcion4'];
+var title='Welcome to React';
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header/>
+        <Header title={title} items={items}/>
         <Content/>
         <Footer/>
       </div>
--- src/components/global/Header.js
+++ src/components/global/Header.js
@@ -1,15 +1,31 @@
+///Dependencias
 import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+
+//Recursos
 import logo from './imagenes/logo.svg'
 import './css/Header.css'
 
 class Header extends Component{
+
+  static PropTypes={
+    title: PropTypes.string.isRequired,
+    items: PropTypes.array.isRequired
+
+  }
+
   render(){
+    const {title, items} = this.props;
     return (
       <div className="header">
         <img src={logo} className="logo" alt="logo" />
           <h3>
-            Welcome to React
-          </h3>   
+            {title}
+          </h3>
+          
+          <ul className="menu">
+            {items.map((item,key) => <li key={key}>{item}</li>  )}
+          </ul>    
       </div>
     );
   }
